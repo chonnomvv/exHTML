2230f9d3c61d27e11c6d9524bc0381815894e23c
chonnomvv <chonnomvv@gmail.com>
2019-04-10 20:35:52 +0900

    line homework 1

---
+++ js/lineCorp.js
@@ -0,0 +1,32 @@
+var btn_lang = $(".btn_lang");
+var list_lang = $(".language-list");
+var body = document.body;
+var submenu = $(".submenu-pr");
+
+
+submenu.on('mouseover focusin', function (e) {
+    var target = $(e.target);
+    var menu_height = $(this).children('.menu-submenu').height();
+    menu_height = menu_height+40;
+
+
+    $(".menu-submenu").css('display', 'none');
+    $(".menu-bg").css('height',menu_height);
+    $(this).children('.menu-submenu').css('display', 'flex');
+});
+
+
+/* 언어 선택 버튼 클릭 동작*/
+btn_lang.on("click", function (e) {
+    e.preventDefault();
+    list_lang.removeClass('hidden-box');
+});
+
+$('body').on('click', function (e) {
+    var target = $(e.target);
+    if (!target.hasClass("btn_lang") && !list_lang.hasClass('hidden-box')) {
+        console.log("hello");
+        list_lang.addClass('hidden-box');
+    }
+});
+
